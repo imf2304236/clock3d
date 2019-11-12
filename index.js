15a7e0ecaba09835ac16be0a2d0e539d1ab78677
Ian F <ianmichael.fennie@haw-hamburg.de>
2019-11-12 08:07:45 +0100

    Initialize canvas

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+'use strict';
 
+// Initialize webGL Renderer
+const canvas = document.getElementById('canvas');
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+renderer.setClearColor('rgb(255, 255, 255)');
+
+// Create scene & camera
+const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper());
+
+const camera = new THREE.PerspectiveCamera(
+    75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 3;
+camera.lookAt(scene.position);
+
+// Add lighting
+const ambientLight = new THREE.AmbientLight(0x909090);
+scene.add(ambientLight);
+const light = new THREE.DirectionalLight(0x444444);
+light.position.set(1.5, 1, 1);
+scene.add(light);
+
+const controls = new THREE.TrackballControls(camera, canvas);
+
+/**
+ * Renders frame
+ */
+function render() {
+  requestAnimationFrame(render);
+
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+render();
