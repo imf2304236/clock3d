b7d88af7a55332a5b4a831953555ba93eb865ccb
Ian F <ianmichael.fennie@haw-hamburg.de>
2019-11-13 12:42:47 +0100

    Add minute ticks

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const bodyRadius = 50;
 const bodyHeight = 5;
 const bodyRadialSegments = 64;
 const bodyHeightSegments = 64;
-const bodyColor = 'gray';
+const bodyColor = 'white';
 const bodyGeometry = new THREE.CylinderBufferGeometry(
     bodyRadius, bodyRadius, bodyHeight, bodyRadialSegments, bodyHeightSegments);
 const bodyMaterial = new THREE.MeshPhongMaterial({color: bodyColor});
@@ -34,8 +34,40 @@ const body = new THREE.Mesh(bodyGeometry, bodyMaterial);
 body.rotateX(Math.PI / 2);
 scene.add(body);
 
-// TODO: Add minute ticks
-// TOOD: Add 5-minute ticks
+// Add minute ticks
+const minuteTickWidth = 1;
+const minuteTickLength = 5;
+const minuteTickColor = 'black';
+for (let i = 0; i != 60; ++i) {
+  const isFiveMinuteMark = (i % 5 == 0);
+  const minuteTickGeometry = isFiveMinuteMark ?
+      new THREE.PlaneBufferGeometry(minuteTickWidth * 2, minuteTickLength * 2) :
+      new THREE.PlaneBufferGeometry(minuteTickWidth, minuteTickLength);
+  const minuteTickMaterial =
+      new THREE.MeshPhongMaterial({color: minuteTickColor});
+  const minuteTick = new THREE.Mesh(minuteTickGeometry, minuteTickMaterial);
+
+  if (isFiveMinuteMark) {
+    minuteTick.position.x =
+        bodyRadius * Math.sin(Math.PI * i / 30) -
+            Math.sin(Math.PI * i / 30) * minuteTickLength;
+    minuteTick.position.y =
+        bodyRadius * Math.cos(Math.PI * i / 30) -
+            Math.cos(Math.PI * i / 30) * minuteTickLength;
+  } else {
+    minuteTick.position.x =
+        bodyRadius * Math.sin(Math.PI * i / 30) -
+            Math.sin(Math.PI * i / 30) * minuteTickLength / 2;
+    minuteTick.position.y =
+        bodyRadius * Math.cos(Math.PI * i / 30) -
+            Math.cos(Math.PI * i / 30) * minuteTickLength / 2;
+  }
+  minuteTick.position.z = bodyHeight / 2;
+
+  minuteTick.rotateZ(-i * Math.PI / 30);
+  scene.add(minuteTick);
+}
+
 // TODO: Mark 12 o'clock position
 // TODO: Add hour-hand
 // TODO: Add minute-hand
